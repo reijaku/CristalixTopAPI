f8390bf38366709e210527ecedecc4fe8328f9fa
Uffyz <atrofimov619@gmail.com>
2026-03-21 19:54:30 +0400

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cristalix",
-    version="0.1.7",
+    version="0.1.8",
     packages=find_packages(),
     install_requires=[
         "curl-cffi",
